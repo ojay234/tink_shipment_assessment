b9fc5df5ef724ce6b9b2a36e3a467fdc645155d9
David Ochuma <davidace998@gmail.com>
2025-02-06 12:33:15 +0100

    updated  app.js

--- shipment-ui/src/App.jsx
+++ shipment-ui/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="app-container">
       <Header />
-      <ShipmentMap shipments={shipments} />
+      <ShipmentMap shipments={dummyShipments} />
       <ShipmentList shipments={shipments} />
     </div>
   );
